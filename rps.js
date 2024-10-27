c3797a105ac3f5d1facb57ae26c125c30f99848d
SeancyG <164585793+SeancyG@users.noreply.github.com>
2024-10-26 22:14:36 -0700

    Refactor game logic except for playRound output.

--- rps.js
+++ rps.js
@@ -7,10 +7,33 @@ const paperValS = 'P';
 const scissorsVal = 'Scissors';
 const scissorsValS = 'S';
 
-const tieMessage = 'A tie. Everyone wins!'
-const rockPaperMessage = 'Paper covers rock.';
-const paperScissorsMessage = 'Scissors cuts paper.';
-const scissorsRockMessage = 'Rock crushes scissors.'
+const initHeader = 'Select an option below.';
+const initTagline = ' ';
+
+const tieHeaderRound = 'Tie';
+const tieTaglineRound = 'Everyone wins!';
+const humanHeaderRound = 'You win!';
+const computerHeaderRound = 'Computer wins...';
+const rockPaperTagline = 'Paper covers rock.';
+const paperScissorsTagline = 'Scissors cuts paper.';
+const scissorsRockTagline = 'Rock crushes scissors.';
+
+const humanHeaderEnd = 'You win!';
+const humanTaglineEnd = 'Let this be a lesson to everyone who ever doubted you.';
+const computerHeaderEnd = 'Computer wins.';
+const computerTaglineEnd = 'Wow, this must be so embarrassing for you. Don\'t worry, I probably won\'t tell anyone.';
+
+let humanScore = 0;
+let computerScore = 0;
+let round = 1;
+
+let maxScore = 5;
+
+let gameComplete = false;
+
+const buttons = document.querySelector('.buttons-container');
+const resultHeader = document.querySelector('h2.result');
+const resultTagline = document.querySelector('p.result');
 
 function getComputerChoice () {
     let choiceNum = Math.random() * 3;
@@ -32,67 +55,87 @@ function getHumanChoice (message = 'Make your choice. "Rock", "Paper", or "Sciss
     return humanChoice;
 }
 
-function playGame() {
-    let humanScore = 0;
-    let computerScore = 0;
-    let round = 1;
+buttons.addEventListener('click', (e) => {
+    switch(e.target.id) {
+        case 'rock':
+            playRound(rockVal, getComputerChoice());
+            break;
+        case 'paper':
+            playRound(paperVal, getComputerChoice());
+            break;
+        case 'scissors':
+            playRound(scissorsVal, getComputerChoice());
+            break;
+    }
+});
 
-    function playRound (humanChoice, computerChoice) {
-        humanChoice = humanChoice.toLowerCase();
-        if (humanChoice.toLowerCase() === rockVal.toLowerCase() || humanChoice.toLowerCase() === rockValS.toLowerCase()) {
-            if (computerChoice === rockVal) {
-                console.log(tieMessage);
-            } else if (computerChoice === paperVal) {
-                console.log(`Computer wins. ${rockPaperMessage}`);
-                computerScore++;
-            } else if (computerChoice === scissorsVal) {
-                console.log(`You win! ${scissorsRockMessage}`);
-                humanScore++;
-            }
-            round ++;
-        } else if (humanChoice.toLowerCase() === paperVal.toLowerCase() || humanChoice.toLowerCase() === paperValS.toLowerCase()) {
-            if (computerChoice === paperVal) {
-                console.log(tieMessage);
-            } else if (computerChoice === scissorsVal) {
-                console.log(`Computer wins. ${paperScissorsMessage}`);
-                computerScore++;
-            } else if (computerChoice === rockVal) {
-                console.log(`You win! ${rockPaperMessage}`);
-                humanScore++;
-            }
-            round ++;
-        } else if (humanChoice.toLowerCase() === scissorsVal.toLowerCase() || humanChoice.toLowerCase() === scissorsValS.toLowerCase()) {
-            if (computerChoice === scissorsVal) {
-                console.log(tieMessage);
-            } else if (computerChoice === rockVal) {
-                console.log(`Computer wins. ${scissorsRockMessage}`);
-                computerScore++;
-            } else if (computerChoice === paperVal) {
-                console.log(`You win! ${paperScissorsMessage}`);
-                humanScore++;
-            }
-            round ++;
-        } else if (humanChoice === '') {
-            console.log('Fine, I\'ll choose for you.')
-            playRound(getComputerChoice(), computerChoice);
-        } else {
-            console.log('I didn\'t understand that. Please try again.');
-            playRound(getHumanChoice(), computerChoice);
+function playRound (humanChoice, computerChoice) {
+    if (gameComplete) {
+        resetGame();
+    }
+
+    humanChoice = humanChoice.toLowerCase();
+
+    if (humanChoice.toLowerCase() === rockVal.toLowerCase() || humanChoice.toLowerCase() === rockValS.toLowerCase()) {
+        if (computerChoice === rockVal) {
+            console.log(tieMessage);
+        } else if (computerChoice === paperVal) {
+            console.log(`Computer wins. ${rockPaperMessage}`);
+            computerScore++;
+        } else if (computerChoice === scissorsVal) {
+            console.log(`You win! ${scissorsRockMessage}`);
+            humanScore++;
         }
-    }
-
-    while (round < 6) {
-        playRound(getHumanChoice(), getComputerChoice());
-    }
-
-    if (humanScore > computerScore) {
-        console.log('You win! Let this be a lesson to everyone who ever doubted you.');
-        console.log(`You: ${humanScore}. Computer: ${computerScore}`);
-    } else if (computerScore > humanScore) {
-        console.log('Wow, this must be so embarrassing for you. Don\'t worry, I probably won\'t tell anyone.');
-        console.log(`Computer: ${computerScore}. You: ${humanScore}.`);
+        round++;
+    } else if (humanChoice.toLowerCase() === paperVal.toLowerCase() || humanChoice.toLowerCase() === paperValS.toLowerCase()) {
+        if (computerChoice === paperVal) {
+            console.log(tieMessage);
+        } else if (computerChoice === scissorsVal) {
+            console.log(`Computer wins. ${paperScissorsMessage}`);
+            computerScore++;
+        } else if (computerChoice === rockVal) {
+            console.log(`You win! ${rockPaperMessage}`);
+            humanScore++;
+        }
+        round++;
+    } else if (humanChoice.toLowerCase() === scissorsVal.toLowerCase() || humanChoice.toLowerCase() === scissorsValS.toLowerCase()) {
+        if (computerChoice === scissorsVal) {
+            console.log(tieMessage);
+        } else if (computerChoice === rockVal) {
+            console.log(`Computer wins. ${scissorsRockMessage}`);
+            computerScore++;
+        } else if (computerChoice === paperVal) {
+            console.log(`You win! ${paperScissorsMessage}`);
+            humanScore++;
+        }
+        round ++;
+    } else if (humanChoice === '') {
+        console.log('Fine, I\'ll choose for you.')
+        playRound(getComputerChoice(), computerChoice);
     } else {
-        console.log(tieMessage);
-        console.log(`You: ${humanScore}. Computer: ${computerScore}`);
+        console.log('I didn\'t understand that. Please try again.');
+        playRound(getHumanChoice(), computerChoice);
     }
+
+    if (humanScore >= maxScore || computerScore >= maxScore) {
+        displayEndMessage();
+        gameComplete = true;
+    }
+}
+
+function displayEndMessage() {
+    if (humanScore > computerScore) {
+        resultHeader.textContent = humanHeaderEnd;
+        resultTagline.textContent = humanTaglineEnd;
+    } else if (computerScore > humanScore) {
+        resultHeader.textContent = computerHeaderEnd;
+        resultTagline.textContent = computerTaglineEnd;
+    } else {
+        resultHeader.textContent = 'A tie?';
+        resultTagline.textContent = 'That wasn\'t supposed to happen.';
+    }
+}
+
+function resetGame() {
+
 }
